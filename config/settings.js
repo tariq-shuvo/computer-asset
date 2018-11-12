815bd86c12a2a6a6be48f875a6491dd53406bc39
Tariqul Islam <shuvo@shikhbeshobai.com>
2018-11-12 19:36:29 +0600

    make it better

--- config/settings.js
+++ config/settings.js
@@ -5,6 +5,6 @@ module.exports = {
    baseURL:"http://localhost:3000/",
    protocol:"http://",
    mongoDB:{
-      link:"mongodb://<test>:<q1w2e3r4t5>@ds019688.mlab.com:19688/userstory10043"
+      link:"mongodb://computer:computer1@ds019688.mlab.com:19688/userstory10043"
    }
 };
