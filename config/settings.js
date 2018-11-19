7937845a8674585315c3a6df08b20a70d67661ca
Tariqul Islam <shuvo@shikhbeshobai.com>
2018-11-19 12:27:40 +0600

    11-19-2018(12:27)

--- config/settings.js
+++ config/settings.js
@@ -2,8 +2,8 @@
  * Created by Shuvo on 11/5/2018.
  */
 module.exports = {
-   baseURL:"http://localhost:3000/",
-   protocol:"http://",
+   baseURL:"https://computer-assets.herokuapp.com/",
+   protocol:"https://",
    mongoDB:{
       link:"mongodb://computer_assets:Hello10043@ds059634.mlab.com:59634/computer-assets"
    }
