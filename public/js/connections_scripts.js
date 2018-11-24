d1407d0effaec72dc3a60cbc6b1da3529eaa90d4
Tariqul Islam <shuvo@shikhbeshobai.com>
2018-11-24 21:51:21 +0600

    Source Network and Destination Network Done

--- public/js/connections_scripts.js
+++ public/js/connections_scripts.js
@@ -14,11 +14,10 @@ $(document).on('change','#select_type',function (e) {
             type: "GET",
             success: function(data) {
                 $(data).each(function (i) {
-                    options += "<option value='"+data[i][type_value]+"' data-id='"+type_value+"'>"+data[i][type_value]+"</option>";
+                    options += "<option value='"+data[i]+"' data-id='computer_name'>"+data[i]+"</option>";
                 });
 
                 $("#select_category").html(options);
-
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 console.log(errorThrown);
@@ -73,7 +72,8 @@ $(document).on('submit','#search_form',function (e) {
 
                 $(".dataBody").html(table);
 
-                $("#mynetwork").css('display','none');
+                $("#mynetwork_source").css('display','none');
+                $("#mynetwork_destination").css('display','none');
 
                 $.getScript( "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js", function( data, textStatus, jqxhr ) {
                     $.getScript( baseURL+"js/jquery.basictable.min.js", function( data, textStatus, jqxhr ) {
@@ -92,7 +92,8 @@ $(document).on('submit','#search_form',function (e) {
 
     if(category_value!=='*' && category_value!==''){
 
-        var nodes_data=[{id: 1, label: category_value}],edges_data=[];
+        var nodes_data_source=[{id: 1, label: category_value}],edges_data_source=[];
+        var nodes_data_destination=[{id: 1, label: category_value}],edges_data_destination=[];
 
 
         $.ajax({
@@ -105,7 +106,8 @@ $(document).on('submit','#search_form',function (e) {
                 category_value:category_value,
             },
             success: function(data) {
-                var count=2;
+                var count_source=2;
+                var count_destination=2;
                 $(data).each(function (i) {
                     table+="<tr>";
                     table+="<td>"+data[i]['date']+"</td>";
@@ -118,35 +120,52 @@ $(document).on('submit','#search_form',function (e) {
                     table+="<td>"+data[i]['source_port']+"</td>";
                     table+="<td>"+data[i]['destination_port']+"</td>";
                     table+="</tr>";
-                    if(data[i]['ip']==data[i]['destination_ip'] || data[i]['ip']==data[i]['source_ip'])
+                    if(data[i]['ip']==data[i]['source_ip'])
                     {
-                        nodes_data.push({id: count, label: data[i]['source_ip']+"\n"+data[i]['destination_ip']+"\n"+data[i]['destination_port']+"\n"+data[i]['protocol']+"\n"+data[i]['tcp_win']});
-                        edges_data.push({from: 1, to: count});
-                        count++;
+                        nodes_data_source.push({id: count_source, label: data[i]['source_ip']+"\n"+data[i]['destination_ip']+"\n"+data[i]['destination_port']+"\n"+data[i]['protocol']+"\n"+data[i]['tcp_win']});
+                        edges_data_source.push({from: 1, to: count_source});
+                        count_source++;
+                    }
+
+                    if(data[i]['ip']==data[i]['destination_ip'])
+                    {
+                        nodes_data_destination.push({id: count_destination, label: data[i]['source_ip']+"\n"+data[i]['destination_ip']+"\n"+data[i]['destination_port']+"\n"+data[i]['protocol']+"\n"+data[i]['tcp_win']});
+                        edges_data_destination.push({from: 1, to: count_destination});
+                        count_destination++;
                     }
 
                 });
 
                 $(".dataBody").html(table);
 
-                $("#mynetwork").css('display','block');
+                $("#mynetwork_source").css('display','block');
+                $("#mynetwork_destination").css('display','block');
 
                 // create an array with nodes
-                var nodes = new vis.DataSet(nodes_data);
+                var nodes_source = new vis.DataSet(nodes_data_source);
+                var nodes_destination = new vis.DataSet(nodes_data_destination);
 
                 // create an array with edges
-                var edges = new vis.DataSet(edges_data);
+                var edges_source = new vis.DataSet(edges_data_source);
+                var edges_destination = new vis.DataSet(edges_data_destination);
 
                 // create a network
-                var container = document.getElementById('mynetwork');
-                var data = {
-                    nodes: nodes,
-                    edges: edges
+                var container_source = document.getElementById('mynetwork_source');
+                var container_destination = document.getElementById('mynetwork_destination');
+                var data_source = {
+                    nodes: nodes_source,
+                    edges: edges_source
                 };
+                var data_destination = {
+                    nodes: nodes_destination,
+                    edges: edges_destination
+                };
+
                 var options = {
                     dragNodes:false
                 };
-                var network = new vis.Network(container, data, options);
+                var network_source = new vis.Network(container_source, data_source, options);
+                var network_destination = new vis.Network(container_destination, data_destination, options);
 
                 $.getScript( "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js", function( data, textStatus, jqxhr ) {
                     $.getScript( baseURL+"js/jquery.basictable.min.js", function( data, textStatus, jqxhr ) {
--- routes/operations.js
+++ routes/operations.js
@@ -132,13 +132,20 @@ router.get('/connection/:hwoption',(req, res, next)=>{
     const column_name = req.params.hwoption;
     const fields ={};
     fields[column_name]=true;
-    Connection.find({},fields,(err, results) => {
-        // Note that this error doesn't mean nothing was found,
-        // it means the database had an error while searching, hence the 500 status
-        if (err) return res.status(500).send(err);
-        // send the list of all people
-        return res.status(200).json(results);
-    });
+    Connection.find().distinct('computer_name', (err, results) => {
+            // Note that this error doesn't mean nothing was found,
+            // it means the database had an error while searching, hence the 500 status
+            if (err) return res.status(500).send(err);
+            // send the list of all people
+            return res.status(200).json(results);
+        });
+    // Connection.find({},fields,(err, results) => {
+    //     // Note that this error doesn't mean nothing was found,
+    //     // it means the database had an error while searching, hence the 500 status
+    //     if (err) return res.status(500).send(err);
+    //     // send the list of all people
+    //     return res.status(200).json(results);
+    // });
 });
 
 router.post('/connection',(req, res, next)=>{
