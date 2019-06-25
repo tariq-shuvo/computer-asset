3380146a8ce58c9aa254234710d6bf2991fc6a9a
Tariqul Islam <shuvo@shikhbeshobai.com>
2019-06-25 23:52:39 +0600

    Computer assets management

--- config/settings.js
+++ config/settings.js
@@ -5,6 +5,6 @@ module.exports = {
    baseURL:"https://computer-assets.herokuapp.com/",
    protocol:"https://",
    mongoDB:{
-      link:"mongodb://computer_assets:Hello10043@ds059634.mlab.com:59634/computer-assets"
+      link:"MongoDatabaseAccessLink"
    }
 };
